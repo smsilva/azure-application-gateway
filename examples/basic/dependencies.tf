723cbee0185cb39c88cf7b68b942d3e82c794a6f
Silvio Silva <silvios.silva-ext3@ab-inbev.com>
2022-03-18 12:29:17 -0300

    refactor(examples/): Simplified examples

---
+++ examples/basic/dependencies.tf
@@ -0,0 +1,17 @@
+resource "azurerm_resource_group" "default" {
+  name     = local.application_gateway_name
+  location = local.location
+}
+
+module "vnet" {
+  source = "git@github.com:smsilva/azure-network.git//src/vnet?ref=3.0.5"
+
+  name                = local.virtual_network_name
+  cidrs               = local.virtual_network_cidrs
+  subnets             = local.virtual_network_subnets
+  resource_group_name = azurerm_resource_group.default.name
+
+  depends_on = [
+    azurerm_resource_group.default
+  ]
+}
--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,34 +1,10 @@
-# Create an Application Gateway ingress controller in Azure Kubernetes Service
-# https://docs.microsoft.com/en-us/azure/developer/terraform/create-k8s-cluster-with-aks-applicationgateway-ingress
-#
-# Secure traffic with a web application firewall (WAF)
-# https://docs.microsoft.com/en-us/azure/aks/operator-best-practices-network#secure-traffic-with-a-web-application-firewall-waf
-
 locals {
   location                 = "eastus2"
   application_gateway_name = "wasp-app-gw-example-1"
-  virtual_network_name     = "vnet-public"
+  virtual_network_name     = "wasp-app-gw-example-1-vnet"
   virtual_network_cidrs    = ["10.244.0.0/14"]
   virtual_network_subnets = [
     { cidr = "10.247.2.0/27", name = "app-gw" }
-  ]
-}
-
-resource "azurerm_resource_group" "default" {
-  name     = local.application_gateway_name
-  location = local.location
-}
-
-module "vnet" {
-  source = "git@github.com:smsilva/azure-network.git//src/vnet?ref=3.0.5"
-
-  name                = local.virtual_network_name
-  cidrs               = local.virtual_network_cidrs
-  subnets             = local.virtual_network_subnets
-  resource_group_name = azurerm_resource_group.default.name
-
-  depends_on = [
-    azurerm_resource_group.default
   ]
 }
 
